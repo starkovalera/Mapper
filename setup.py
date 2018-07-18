6f8e2aa7b6d01ad08b0c7794f5908a8f9dbe5702
Валерия Старкова <starkovalera@gmail.com>
2018-07-19 00:53:03 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pipeline',
+      version='0.1',
+      description='Pipeline to read RSS feed to Django model',
+      url='https://github.com/starkovalera/Pipeline',
+      author='starkovalera',
+      packages=['pipeline', 'pipeline.reader', 'pipeline.mapper', 'pipeline.writer'],
+      install_requires=[
+          'feedparser',
+          'pytest',
+          'python>=3.6.0'
+      ])
