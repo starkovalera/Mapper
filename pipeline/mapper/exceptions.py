282b995e6c354b161d35c6bf0ea0dd1bd211294f
Валерия Старкова <starkovalera@gmail.com>
2018-07-18 17:52:47 +0300

    Rule: variable number of arguments

--- pipeline/mapper/exceptions.py
+++ pipeline/mapper/exceptions.py
@@ -1,1 +1,3 @@
+class InvalidRuleConfigurationException(Exception):
+    """"""
 
--- pipeline/mapper/rule.py
+++ pipeline/mapper/rule.py
@@ -1,3 +1,6 @@
+from .exceptions import *
+
+
 class Rule:
     def __init__(self, keys_from, keys_to=None, transform_function=None):
         self.keys_from = keys_from
@@ -12,3 +15,23 @@ class Rule:
             if len(keys) == 1:
                 values = values,
         data.update(zip(keys, values))
+
+    def _resolve_arguments(self, keys_from, args):
+        if not args or len(args) > 2:
+            raise InvalidRuleConfigurationException()
+        keys_from = self._handle_as_tuple(keys_from)
+        if len(args) == 2:
+            if not callable(args[1]):
+                raise InvalidRuleConfigurationException()
+            keys_to, transform_function = self._handle_as_tuple(args[0]), args[1]
+        elif callable(args[0]):
+            keys_to, transform_function = None, args[0]
+        else:
+            keys_to, transform_function = self._handle_as_tuple(args[0]), None
+            if len(keys_to) != len(keys_from):
+                raise InvalidRuleConfigurationException()
+        return keys_from, keys_to, transform_function
+
+    @staticmethod
+    def _handle_as_tuple(arg):
+        return arg if isinstance(arg, tuple) else (arg,)
